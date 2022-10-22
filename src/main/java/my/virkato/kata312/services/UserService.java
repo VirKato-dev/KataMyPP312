29ef4486a8396845e7e975ab85d9694f3b8a4bdf
VirKato <dascat79@mail.ru>
2022-10-22 08:35:46 +0700

    задача 3.1.4 REST контроллеры

--- src/main/java/my/virkato/kata312/services/UserService.java
+++ src/main/java/my/virkato/kata312/services/UserService.java
@@ -4,6 +4,7 @@ import my.virkato.kata312.entities.UserEntity;
 import my.virkato.kata312.repositories.UserDao;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,9 +16,11 @@ import java.util.Collection;
 public class UserService implements UserDetailsService {
 
     private final UserDao dao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, PasswordEncoder passwordEncoder) {
         dao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -27,6 +30,14 @@ public class UserService implements UserDetailsService {
 
     @Transactional
     public void createOrUpdate(UserEntity user) {
+        UserEntity oldUser = get(user.getId());
+        user.setUsername(oldUser.getUsername());
+        if (!oldUser.getPassword().equals(user.getPassword()) &&
+                !"".equals(user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        } else {
+            user.setPassword(oldUser.getPassword());
+        }
         dao.save(user);
     }
 
